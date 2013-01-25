4b036d9247ab39160e55d52c3ea39dd93a266786
Michael <michael.kozakov@gmail.com>
2013-01-24 19:59:54 -0500

    Added click listener for submit button

--- Assignment 1/scripts/comments.js
+++ Assignment 1/scripts/comments.js
@@ -1,1 +1,8 @@
 //javascript goes here
+$(document).ready(function(){
+	var postComment = function() {
+		alert("posting comment");
+	}
+	var submitBtn = $(".submit-comment-btn").click(postComment);
+	//TODO: replace with AJAX request
+});

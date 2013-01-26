270b9be74d7e5806bdfbf0765b3d6b33b443e150
Michael <michael.kozakov@gmail.com>
2013-01-26 00:27:07 -0500

    Created a basic comment system and added CSS file

--- Assignment 1/scripts/comments.js
+++ Assignment 1/scripts/comments.js
@@ -1,8 +1,44 @@
-//javascript goes here
 $(document).ready(function(){
-	var postComment = function() {
-		alert("posting comment");
-	}
-	var submitBtn = $(".submit-comment-btn").click(postComment);
-	//TODO: replace with AJAX request
+    var createThread = function(container, commentText, isRoot) {
+        var comment = jQuery('<div/>', {
+            'class': 'comment' + (isRoot ? ' root-comment' : ""),
+            'text': commentText
+        });
+        var replyButton = jQuery('<a/>', {
+            'href' : '#',
+            'class': 'reply-button',
+            'text': "reply"
+        });
+        var repliesContainer = jQuery('<div/>', {
+            'class': 'replies-container'
+        });
+
+        container.append(comment, replyButton, repliesContainer);
+        replyButton.click(function() {
+            $('.comment-editor').hide();
+            $('.reply-button').show();
+            var commentEditor = jQuery('<div/>', {
+                'class': 'comment-editor'
+            });
+            var textBox = jQuery('<textarea/>', {
+                'class': 'comment-box',
+                'maxlength': 140
+            });
+            var saveCommentBtn = jQuery('<button>save</button>', {
+                'class': 'save-reply-button'
+            });
+            commentEditor.append(textBox, saveCommentBtn);
+            repliesContainer.append(commentEditor);
+            replyButton.hide();
+            saveCommentBtn.click(function() {
+                if(textBox.val()) {
+                    commentEditor.remove();
+                    replyButton.show();
+                    createThread(repliesContainer, textBox.val());
+                }
+            });
+        });
+        isRoot && replyButton.click();
+    };
+    createThread($("#thread"), "I am Jack's Complete Lack of Surprise. AMA", true);
 });

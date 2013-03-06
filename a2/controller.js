b9eaed8cfa8e469f90297b5485239dd699a8c8ed
Michael <michael.kozakov@gmail.com>
2013-03-06 02:37:57 -0500

    Created an express app for routing, and added dummy files

---
+++ a2/controller.js
@@ -0,0 +1,9 @@
+exports.follow = function(req, res) {
+    //TODO: write follow method, which adds req.param("blog") to the list of tracked blogs
+};
+
+exports.getTrends = function(req, res) {
+  //TODO: write getTrends method
+  // use the paramters req.query.limit and req.query.order
+  // if req.params.blogHostname is specified, only show trends for that blog
+};

24705d16984f69580796a3361ad69bd14419ed41
NatMorcos <n.morcos@mail.utoronto.ca>
2013-03-27 13:41:05 -0400

    setting up project files

---
+++ a3/public/js/custom.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Owner
+ * Date: 27/03/13
+ * Time: 11:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function(){
+
+});

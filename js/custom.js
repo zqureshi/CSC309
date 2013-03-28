eefe10a8f8121e5ba1ff6952bd9547b45b07f61e
Zeeshan Qureshi <zee@zqureshi.in>
2013-03-28 01:45:41 -0400

    Update to natalie's latest commit.

--- js/custom.js
+++ js/custom.js
@@ -11,21 +11,37 @@ $(document).ready(function(){
     var favourites = []; //will hold JSON
     var loadIndex = 0;   //index first undisplayed tweet
 
-    /*load the tweets from the local file*/
-    $.getJSON('./favs.json', function(data){
-        favourites.push(data);
-    });
-
     /** Takes a tweet object from the JSON array favourites and builds then
      * returns a div containing to relevant information.
      * @param {JSON} tweetObject
      * @returns {*|jQuery|HTMLElement}
      */
     var buildTweet = function(tweetObject){
-        var tweetContainer = $('<div/>', {
+        var tweetContainer = $('<li/>', {
             'class':'tweet-container'
         });
-        //TODO: implement
+        var link = $('<a/>', {
+            'href':'#'
+        });
+        var img = $('<img/>', {
+            'src' : tweetObject.user.profile_image_url
+            //TODO image resizing in both layouts
+        });
+        var name = $('<h2/>', {
+            'html': tweetObject.user.name
+        });
+        var handler = $('<span/>', {
+            'class':'user-handler',
+            'html': '@' + tweetObject.user.screen_name
+        });
+        var text = $('<p/>', {
+            'html': tweetObject.text
+            //TODO parse the text for @handlers and links
+        });
+
+        name.append(handler);
+        link.append(img, name, text);
+        tweetContainer.append(link);
         return tweetContainer;
     }
 
@@ -34,10 +50,39 @@ $(document).ready(function(){
      */
     var populateMain = function(){
            var i = 0;
-           while(i<10){
+           while( (i < 10) & (loadIndex + i < favourites.length) ){
                var newTweet = buildTweet(favourites[loadIndex + i]);
-               //TODO append new Tweet to display
+
+               /* id=index in array to easily build user profile on tweet click*/
+               newTweet.attr('id', loadIndex + i);
+
+               $(newTweet).appendTo("#tweetList");
                i++;
+
+               /*if(loadIndex + i < favourites.length){
+               //TODO find something cheeky to put at the end
+                   $('.ui-block-a').append("<div><p>That's all folks!</p></div>"); //will only execute once
+               } */
            }
+           $('#tweetList').listview('refresh')
     }
+
+    /*load the tweets from the local file*/
+    $.getJSON('./favs.json', function(data){
+        favourites = data;
+        populateMain();
+    });
+
+
+    //TODO implement: event handler for scrolling, call populateMain() on scroll down.
+    //$(something).something(function(){ populateMain(); }
+
+    //TODO implement event handler for click on chevron in list view , expands user info
+    /* this guy: <span class="ui-icon ui-icon-arrow-r ui-icon-shadow"> </span>
+    the <li> element it's in has and id # that is the index of the tweetObject in the JSON array
+    to make looking up user info easier */
+
+    //TODO implement lightbox popup for links to pictures
+
+
 });
